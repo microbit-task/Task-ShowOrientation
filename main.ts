8161245bf65d827c1e67799b399fb4b137159c8a
tampopo <67646094+tampopo256@users.noreply.github.com>
2021-06-22 23:53:50 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,43 @@
+basic.forever(function () {
+    if (input.compassHeading() < 45) {
+        basic.showLeds(`
+            # . . . #
+            # # . . #
+            # . # . #
+            # . . # #
+            # . . . #
+            `)
+    } else if (input.compassHeading() < 135) {
+        basic.showLeds(`
+            # # # # .
+            # . . . .
+            # # # # .
+            # . . . .
+            # # # # .
+            `)
+    } else if (input.compassHeading() < 225) {
+        basic.showLeds(`
+            # # # # .
+            # . . . .
+            # # # # .
+            . . . # .
+            # # # # .
+            `)
+    } else if (input.compassHeading() < 315) {
+        basic.showLeds(`
+            # . # . #
+            # . # . #
+            # . # . #
+            . # . # .
+            . # . # .
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            # # . . #
+            # . # . #
+            # . . # #
+            # . . . #
+            `)
+    }
+})
